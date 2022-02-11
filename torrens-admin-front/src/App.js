d340ffd7fcedce88983f693a662eed6dbfcf1fa2
Edgar Junior Otero Rada <dgrotero697@gmail.com>
2022-02-10 21:11:50 -0500

    implementacion del sidebar

--- torrens-admin-front/src/App.js
+++ torrens-admin-front/src/App.js
@@ -1,14 +1,16 @@
-import './App.css';
+import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-
 import { Route, Switch } from "react-router";
-import Login from './pages/login';
-import { Register } from './pages/register';
+
+import Login from "./pages/login";
+import { Register } from "./pages/register";
+import DashBoard from "./pages/dashboard";
 
 function App() {
   return (
     <div className="App">
-     <Switch>
+      <Switch>
+        <Route exact path="/inicio" component={DashBoard} />
         <Route exact path="/register" component={Register} />
         <Route exact path="/" component={Login} />
       </Switch>
